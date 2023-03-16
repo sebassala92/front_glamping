a026c6e59a5ec52ef6803ccce32f9b02a37ea450
Sebastián Salazar <zebasz_07@hotmail.com>
2023-03-16 00:25:08 -0500

    commit changes front glamping 6

--- src/Landing/js/LandingDomo1.js
+++ src/Landing/js/LandingDomo1.js
@@ -3,6 +3,7 @@ import React from 'react'
 import MainBanner from "../../components/js/MainBanner";
 import TextWrapper3 from "../../components/js/TextWrapper3";
 import ContentWrapper from "../../components/js/ContentWrapper";
+import PhotoWrapper from "../../components/js/PhotoWrapper";
 
 const Landing2 = () => {
 
@@ -11,6 +12,15 @@ const Landing2 = () => {
     urlImage: '../img/domo1/domo1.png',
     urlImage2: '../img/domo1/domo1.png',
     text: '¡Experimenta la comodidad y el lujo en plena naturaleza con nuestro Domo Glamping. Este alojamiento único te permitirá disfrutar de las estrellas desde la comodidad de tu propia cama, gracias a su techo transparente. Equipado con todas las comodidades necesarias, el Domo Glamping es perfecto para aquellos que buscan una experiencia de camping sin sacrificar la comodidad.',
+    photoList : {
+      photo_1: '../img/domo1/photo1.png',
+      photo_2: '../img/domo1/photo2.png',
+      photo_3: '../img/domo1/photo3.png',
+      photo_4: '../img/domo1/photo4.png',
+      photo_5: '../img/domo1/photo5.png',
+      photo_6: '../img/domo1/photo6.png',
+      photo_7: '../img/domo1/photo7.png'
+    }
   }
 
   return (
@@ -18,7 +28,7 @@ const Landing2 = () => {
         <MainBanner name_url={props.name} />
         <TextWrapper3 />
         <ContentWrapper name={props.name} urlImage={props.urlImage} urlImage2={props.urlImage2} text={props.text}/>
-      
+        <PhotoWrapper photoList={props.photoList} />
     </div>
   )
 }
--- src/components/js/PhotoWrapper.js
+++ src/components/js/PhotoWrapper.js
@@ -1,1 +1,22 @@
+import React from 'react'
 
+import classes from '../scss/PhotoWrapper.module.scss'
+
+const PhotoWrapper = (props) => {
+
+  return (
+    <article className={classes.photo_wrapper}>
+
+        <div className={classes.cont_1}>
+            {
+
+            props.photoList[1]
+
+            }
+        </div>
+      
+    </article>
+  )
+}
+
+export default PhotoWrapper
