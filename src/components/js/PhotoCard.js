dd46d965e39b8453996197d950d934d81d90a442
Sebastián Salazar <zebasz_07@hotmail.com>
2023-03-21 23:58:37 -0500

    commit changes front glamping 19

--- src/components/js/PhotoCard.js
+++ src/components/js/PhotoCard.js
@@ -15,23 +15,20 @@ const PhotoCard = () => {
         <div className={classes.cont_2}>
             <div className={classes.cont_21}>4.5</div>
             <div className={classes.cont_22}>1262 Reviews</div>
+            <div className={classes.cont_23}>$300</div>
         </div>
 
-        <div className={classes.cont_3}>$300</div>
 
-        <div className={classes.cont_4}>
-            <div className={classes.cont_41}></div>
-            <div className={classes.cont_42}>save upto 70% off</div>
+        <div className={classes.cont_3}>
+            <div className={classes.cont_31}></div>
+            <div className={classes.cont_32}>save upto 70% off</div>
+            <div className={classes.cont_33}>$267</div>
             
         </div>
 
-        <div className={classes.cont_5}>
-            <div className={classes.cont_51}>$267</div>
-        </div>
-
-        <div className={classes.cont_6}>
-            <div className={classes.cont_61}></div>
-            <div className={classes.cont_62}></div>
+        <div className={classes.cont_4}>
+            <div className={classes.cont_41}></div>
+            <div className={classes.cont_42}>98 people viewing now</div>
         </div>
       
     </div>
