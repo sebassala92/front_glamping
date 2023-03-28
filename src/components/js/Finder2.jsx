aaf199658eb27d40c643692ff2de00b24be2cae8
Sebastián Salazar <zebasz_07@hotmail.com>
2023-03-28 00:02:11 -0500

    commit changes front glamping 28

--- src/components/js/Finder2.jsx
+++ src/components/js/Finder2.jsx
@@ -13,39 +13,23 @@ const Finder2 = () => {
   return (
     <article className={classes.finder2}>
 
+
+
+      <div className={classes.cont_2}>
+        <div className={classes.cont_3}>
+          <Calendar onChange={onChange} value={value} />
+        </div>
+
+      </div>
+
       <div className={classes.search}>
-
-        <div className={classes.cont_1}>
-
-          <p className={classes.title}>Inicio estadía y finalización</p>
-
-          <div className={classes.display}></div>    
-
-        </div>
-
-        <div className={classes.cont_1}>
-
-          <p className={classes.title}>Cantidad de personas</p>
-
-          <div className={classes.display}></div>    
-                
-        </div>
 
         <div className={classes.cont_1}>
 
           <p className={classes.space}></p>
 
           <div className={classes.btn}>BUSCAR</div>
- 
                 
-        </div>
-
-
-      </div>
-
-      <div className={classes.cont_2}>
-        <div className={classes.cont_3}>
-          <Calendar onChange={onChange} value={value} />
         </div>
 
       </div>
