ab077ef6434697ddfbd87a8241c80f28be926d0f
Sebastián Salazar <zebasz_07@hotmail.com>
2023-04-03 10:32:31 -0500

    commit changes front glamping 37

--- src/components/js/ContactCard.jsx
+++ src/components/js/ContactCard.jsx
@@ -28,48 +28,48 @@ const ContactCard = () => {
 
         <div className={classes.cont_12}></div>
 
-          <div className={classes.cont_13}>
+        <div className={classes.cont_13}>
 
-          <h2 className={classes.titulo}>Información</h2>
+        <h2 className={classes.titulo}>Información</h2>
 
-            <div className={classes.info}>
+          <div className={classes.info}>
 
-              <span className={classes.logo__mail}></span>
-              <span className={classes.text}>carlosjim84@hotmail.com</span>
-
-            </div>
-
-            <div className={classes.info}>
-
-              <span className={classes.logo__wp}></span>
-              <span className={classes.text}>+57 312 2941890</span>
-
-            </div>
-
-            <div className={classes.info}>
-
-              <span className={classes.logo__clock}></span>
-              <span className={classes.text}>7:00 - 16:00</span>
-
-            </div>
-
-            <div className={classes.info}>
-
-              <span className={classes.logo__place}></span>
-              <span className={classes.text}>Granada - Antioquia</span>
-
-            </div>
+            <span className={classes.logo__mail}></span>
+            <span className={classes.text}>carlosjim84@hotmail.com</span>
 
           </div>
 
-          <div className={classes.cont_14}></div>
+          <div className={classes.info}>
 
-          <div className={classes.cont_14__2}>
-              <span className={classes.logo}></span>
-              <span className={classes.text}></span>
+            <span className={classes.logo__wp}></span>
+            <span className={classes.text}>+57 312 2941890</span>
+
+          </div>
+
+          <div className={classes.info}>
+
+            <span className={classes.logo__clock}></span>
+            <span className={classes.text}>7:00 - 16:00</span>
+
+          </div>
+
+          <div className={classes.info}>
+
+            <span className={classes.logo__place}></span>
+            <span className={classes.text}>Granada - Antioquia</span>
+
           </div>
 
         </div>
+
+        <div className={classes.cont_14}></div>
+
+        <div className={classes.cont_14__2}>
+            <span className={classes.logo}></span>
+            <span className={classes.text}></span>
+        </div>
+
+      </div>
 
       
     </div>
